680ef9a8bf193110f2a6e1bc329eebcd480519e1
Stefan Kremser <stef.1497@gmail.com>
2019-06-26 13:23:49 +0200

    Reworked everything
    - New style
    - More easter egg profile pics
    - Scaled down project pics
    - Redesigns and merged support and donate

--- js/space.js
+++ js/space.js
@@ -25,6 +25,28 @@ function drawing() {
 
 drawing();
 
-window.onload = function() {
+function changeImage() {
+  document.getElementById("spacehuhngallery").src = "img/gallery/" + (Math.floor(Math.random() * (38 + 1))) + ".jpg";
+}
 
+function copyToClipboard(element) {
+  var str = document.getElementById(element).innerHTML;
+
+  const el = document.createElement('textarea');
+  el.value = str;
+  el.setAttribute('readonly', '');
+  el.style.position = 'absolute';
+  el.style.left = '-9999px';
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+}
+
+
+window.onload = function() {
+  document.getElementById("profilepic").addEventListener('click', changeImage);
+  document.getElementById("btc").addEventListener('click', copyToClipboard);
+  document.getElementById("ltc").addEventListener('click', copyToClipboard);
+  document.getElementById("eth").addEventListener('click', copyToClipboard);
 };
