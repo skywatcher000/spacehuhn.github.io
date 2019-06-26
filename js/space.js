9701fac0ac31cb015b5aa00997e6474ab58fce11
Stefan Kremser <stef.1497@gmail.com>
2019-06-26 17:07:22 +0200

    Moved js

--- js/space.js
+++ js/space.js
@@ -1,30 +1,3 @@
-/* Borrowed from here https://codepen.io/AlienPiglet/pen/hvekG */
-function drawing() {
-  var c = document.getElementById('sky');
-  var ctx = c.getContext('2d');
-  var xMax = c.width = window.screen.availWidth;
-  var yMax = c.height = window.screen.availHeight;
-
-  var hmTimes = Math.round(xMax + yMax);
-
-  for (var i = 0; i <= hmTimes; i++) {
-    var randomX = Math.floor((Math.random() * xMax) + 1);
-    var randomY = Math.floor((Math.random() * yMax) + 1);
-    var randomSize = Math.floor((Math.random() * 2) + 1);
-    var randomOpacityOne = Math.floor((Math.random() * 9) + 1);
-    var randomOpacityTwo = Math.floor((Math.random() * 9) + 1);
-    var randomHue = Math.floor((Math.random() * 360) + 1);
-    if (randomSize > 1) {
-      ctx.shadowBlur = Math.floor((Math.random() * 15) + 5);
-      ctx.shadowColor = "white";
-    }
-    ctx.fillStyle = "hsla(" + randomHue + ", 30%, 80%, ." + randomOpacityOne + randomOpacityTwo + ")";
-    ctx.fillRect(randomX, randomY, randomSize, randomSize);
-  }
-}
-
-drawing();
-
 function changeImage() {
   document.getElementById("spacehuhngallery").src = "img/gallery/" + (Math.floor(Math.random() * (38 + 1))) + ".jpg";
 }
